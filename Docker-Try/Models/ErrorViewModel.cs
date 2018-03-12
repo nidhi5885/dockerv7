72ed721d0d5c53cf57695cc8a809ffd120487a25
Nidhi Sharma <nidhisharma5885@gmai.com>
2018-03-12 10:32:54 +0530

    Initial commit

---
+++ Docker-Try/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Docker_Try.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
